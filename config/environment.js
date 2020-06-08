402931bb2a57dc63f0afb5dd59c41c7c7ece648f
bmoura <bruno.moura@lbca.com.br>
2020-06-07 23:25:15 -0300

    Remote new

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'ember-quickstart',
     environment,
-    rootURL: '/',
+    rootURL: '/other/ember-quickstart/dist',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
